8321611266d625591f0982f791f98d2e03bb5c5c
Sebastian RODRIGUEZ <sebastian.rodriguez@rmit.edu.au>
2026-03-27 17:32:19 +1100

    feat: add jetbrains mono fonts

--- flake.nix
+++ flake.nix
@@ -13,8 +13,13 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.neovim
+        [
+          pkgs.neovim
         ];
+
+      fonts.packages = [
+        pkgs.nerd-fonts.jetbrains-mono
+      ];
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
