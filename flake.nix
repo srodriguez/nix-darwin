c0adf4b31224b405b2212a56bf5687c68f67aad7
Sebastian RODRIGUEZ <sebastian.rodriguez@rmit.edu.au>
2026-04-08 11:44:45 +1000

    feat: add home module to nix-darwin flake.

--- flake.nix
+++ flake.nix
@@ -29,30 +29,6 @@
         pkgs.nerd-fonts.jetbrains-mono
       ];
 
-      system.primaryUser = "srodriguez";
-
-      # See documentation : https://mynixos.com/
-      # https://mynixos.com/nix-darwin/options
-      # https://mynixos.com/nix-darwin/options/system.defaults
-      system.defaults = {
-        dock.autohide = true;
-        dock.orientation = "right";
-        #top right corner hot action : 4: Desktop
-        dock.wvous-tr-corner = 4;
-        dock.persistent-apps = [
-          "/Applications/Nix Apps/Firefox.app"
-          "/Applications/Nix Apps/WezTerm.app"
-          "/System/Applications/Reminders.app"
-          "/Applications/Microsoft Teams.app"
-          "/Applications/Nix Apps/Obsidian.app"
-          "/Applications/Nix Apps/Zotero.app"
-        ];
-
-        finder.FXPreferredViewStyle = "clmv";
-        loginwindow.GuestEnabled = false;
-
-      };
-
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
@@ -78,6 +54,7 @@
       specialArgs = { username = "srodriguez"; };
       modules = [
         configuration 
+        ./modules/home.nix
         ./modules/terminal.nix
         ./modules/dev-common.nix
         ./modules/desktop-tools.nix
